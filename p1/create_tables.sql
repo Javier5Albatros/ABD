7cdfba36dda524ba2a0fc08fa7b89842e18322b5
Abortos <javieralvarez2.ja@gmail.com>
2021-10-05 10:10:34 +0200

    Matriculados table

--- p1/create_tables.sql
+++ p1/create_tables.sql
@@ -1,3 +1,23 @@
+CREATE TABLE IF NOT EXISTS `PracABD1`.`personas` (
+  `PersonaID` INT NOT NULL,
+  `Dni` INT UNIQUE NOT NULL,
+  `Nombre` VARCHAR(20) NOT NULL,
+  `Apellido` VARCHAR(30) not NULL,
+  `Genero` VARCHAR(1) NOT NULL,
+  `Direccion` VARCHAR(60) NULL,
+  `Localidad` VARCHAR(50) NULL,
+  `Provincia` VARCHAR(30) NULL,
+  `CodPostal` INT NOT NULL,
+  `Telefono` VARCHAR(9) NULL,
+  `EnParo` BOOLEAN NOT NULL,
+  `Canal` ENUM('0' , '1','2','3','4'),
+  `FechaNac` DATE NOT NULL,
+	`Email` VARCHAR(60) NOT NULL,
+  PRIMARY KEY (`PersonaID`),
+  UNIQUE INDEX `PersonaID_UNIQUE` (`PersonaID` ASC) VISIBLE)
+  TABLESPACE ABDDBA_TBLS_personas
+ENGINE = InnoDB;
+
 CREATE TABLE IF NOT EXISTS `PracABD1`.`cursos` (
   `CursoID` INT NOT NULL,
   `nombre` VARCHAR(15) NOT NULL,
@@ -8,22 +28,13 @@ CREATE TABLE IF NOT EXISTS `PracABD1`.`cursos` (
   TABLESPACE ABDDBA_TBLS_cursos
 ENGINE = InnoDB;
 
-CREATE TABLE IF NOT EXISTS `PracABD1`.`personas` (
+CREATE TABLE IF NOT EXISTS `PracABD1`.`matriculados_interesados` (
   `PersonaID` INT NOT NULL,
-  `Dni`int unique not null,
-  `Nombre` VARCHAR(20) NOT NULL,
-  `Apellido` VARCHAR(30) not NULL,
-  `Genero` varchar(1) NOT NULL,
-   `Direccion` VARCHAR(60) NULL,
-    `Localidad` VARCHAR(50) NULL,
-     `Provincia` VARCHAR(30) NULL,
-     `CodPostal`int not null,
-     `Telefono` varchar(9) null,
-     `EnParo`  boolean not null,
-     `Canal` enum('0' , '1','2','3','4'),
-     `FechaNac` date not null,
-	 `Email` varchar(60) not null,
-  PRIMARY KEY (`PersonaID`),
-  UNIQUE INDEX `PersonaID_UNIQUE` (`PersonaID` ASC) VISIBLE)
-  TABLESPACE ABDDBA_TBLS_personas
-ENGINE = InnoDB;
+  `CursoID` INT NOT NULL,
+  `matriculado` TINYINT NULL,
+  `comentarios` VARCHAR(500) NULL,
+  PRIMARY KEY (`PersonaID`, `CursoID`),
+  FOREIGN KEY (PersonaID) REFERENCES personas(PersonaID),
+  FOREIGN KEY (CursoID) REFERENCES cursos(CursoID))
+  TABLESPACE ABDDBA_TBLS_matriculados
+ENGINE = InnoDB
