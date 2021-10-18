a2610b6cc4299620f0568aae5978c43074d307db
Abortos <javieralvarez2.ja@gmail.com>
2021-10-18 11:56:26 +0200

    Queries

---
+++ p1/queries.sql
@@ -0,0 +1,12 @@
+SELECT DISTINCT email from personas WHERE nombre LIKE
+'Ángeles' AND apellidos LIKE 'del Nido';
+
+SELECT DISTINCT email from personas WHERE enParo = 0;
+
+SELECT COUNT(*) FROM personas WHERE provincia LIKE 'Lugo';
+
+SELECT personas.nombre, apellidos, area, comentarios FROM 
+personas INNER JOIN matriculados_interesados ON matriculados_interesados.PersonaID = personas.PersonaID
+INNER JOIN cursos ON cursos.CursoID = matriculados_interesados.CursoID
+WHERE provincia LIKE 'Madrid' AND edicion = 2018
+ORDER BY apellidos DESC
