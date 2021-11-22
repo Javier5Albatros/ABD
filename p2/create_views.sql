91bd8922267c152a409e7361928e76c58f743fcb
Abortos <javieralvarez2.ja@gmail.com>
2021-11-22 11:21:36 +0100

    Vistas de mierda

--- p2/create_views.sql
+++ p2/create_views.sql
@@ -1,7 +1,9 @@
 CREATE VIEW personas_suplencia AS 
-    SELECT PersonaID, Nombre, Apellidos, Provincia, Telefono, Email FROM personas;
+    SELECT * FROM personas;
 
 CREATE VIEW cursos_suplencia AS 
     SELECT CursoID, Nombre, Area FROM cursos WHERE edicion LIKE 2020;
+
+GRANT INSERT ON  TO comercial;
 
 ALTER TABLE cursos MODIFY COLUMN Edicion INT NOT NULL DEFAULT 2020;
--- p2/suplencia.sql
+++ p2/suplencia.sql
@@ -1,5 +1,6 @@
 CREATE USER Suplencia IDENTIFIED BY 'daniapruebanos';
 GRANT SELECT ON pracabd1.cursos_suplencia TO Suplencia;
-GRANT SELECT ON pracabd1.personas_suplencia TO Suplencia; 
 GRANT INSERT ON pracabd1.cursos_suplencia TO Suplencia;
+
+GRANT SELECT(PersonaID, Nombre, Apellidos, Provincia, Telefono, Email) ON pracabd1.personas_suplencia TO Suplencia; 
 GRANT INSERT ON pracabd1.personas_suplencia TO Suplencia; 
